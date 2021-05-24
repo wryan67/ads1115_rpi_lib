ffba730e0934e75a97159a71f1fdf261eb0f0e39
wade ryan <wryan67@gmail.com>
2021-05-24 17:13:19 -0500

    continous operation

--- example/knobtest.cpp
+++ example/knobtest.cpp
@@ -110,7 +110,7 @@ int main(int argc, char **argv)
 
     int startTime=currentTimeMillis();
     for (int i=0;i<4;++i) {
-      float v=readVoltage(ADS1115_HANDLE, i, gain);
+      float v=readVoltageSingleShot(ADS1115_HANDLE, i, gain);
       if (v>6) {
        v=0;
       }
