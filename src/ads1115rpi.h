bde8369fc2136bf79188c31651f951491a61f62a
wade ryan <wryan67@gmail.com>
2021-05-24 12:48:10 -0500

    continous operation

--- src/ads1115rpi.h
+++ src/ads1115rpi.h
@@ -32,7 +32,7 @@ struct adsConfig {
      
 };
 
-int   setADS1115Config(struct adsConfig config);
+int   setADS1115Config(int handle, struct adsConfig config);
 float getADS1115MaxGain(int gain);
 int   getADSampleRate(int sps);
 float readVoltage(int ADS1115_HANDLE);
