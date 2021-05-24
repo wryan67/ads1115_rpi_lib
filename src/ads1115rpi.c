b5acfc4652e44d12cbedfd845bafcce0edd75366
wade ryan <wryan67@gmail.com>
2021-05-24 17:54:26 -0500

    continous operation

--- src/ads1115rpi.c
+++ src/ads1115rpi.c
@@ -205,6 +205,7 @@ void setSingeShotSingleEndedConfig(int handle, int pin, int gain) {
     low |= (0x01 && configuration.latchingComparator)  << 2;  // 1 bit  
     low |= (0x01 && configuration.comparatorQueue)     << 0;  // 2 bits  
 
+    fprintf(stderr,"hi: 0x%02x (hard values only)\n", high & 0xc1);
     fprintf(stderr,"hi: 0x%02x\n",high);
     fprintf(stderr,"lo: 0x%02x\n",low);
     fprintf(stderr,"config: 0x%0x\n", ((high<<8)|low));
