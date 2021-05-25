9639ae09f32719ea0b42bfa4ebe9bc79447d1415
wade ryan <wryan67@gmail.com>
2021-05-25 02:10:21 -0500

    continous operation

--- src/ads1115rpi.c
+++ src/ads1115rpi.c
@@ -159,9 +159,6 @@ void  setADS1115ContinuousMode(int handle, int channel, int gain, int sps) {
     if (debug) fprintf(stderr,"set continuous mode channel=%d, gain=%d, sps=%d\n",
                                 channel, gain, sps);
 
- // set hi/lo threshold register
-    setThreshold(handle, ADS1115_HiThresholdRegister, (uint16_t)0xFFFF);
-    setThreshold(handle, ADS1115_LoThresholdRegister, (uint16_t)0x0000);
 
 
     setADS1115Config(handle, config);
@@ -169,6 +166,9 @@ void  setADS1115ContinuousMode(int handle, int channel, int gain, int sps) {
     // wiringPiI2CWriteReg16(handle, ADS1115_ConversionRegister, 0x00);
     // delay(1);
 
+ // set hi/lo threshold register
+    setThreshold(handle, ADS1115_HiThresholdRegister, (uint16_t)0xFFFF);
+    setThreshold(handle, ADS1115_LoThresholdRegister, (uint16_t)0x0000);
 
 
 
